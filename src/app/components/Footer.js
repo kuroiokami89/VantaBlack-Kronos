20316fb0337fcff92cb6d7efdad20f75c334f36a
kuroiokami89 <46483858+kuroiokami89@users.noreply.github.com>
2025-10-14 23:06:26 +0200

    Fixed footer and removed useless classes

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -6,20 +6,17 @@ export default function Footer() {
   return (
     <>
       <footer id="footer" className={`borders ${NeutralFace.className}`}>
-        <div className="footer-link">
-          <div className="social-links">
-            <a
-              className="link-btn"
-              href="https://www.linkedin.com/in/sergio-allushaj-a92b9717a/"
-            >
-              INSTAGRAM
-            </a>
-            <a className="link-btn link" href="https://github.com/kuroiokami89">
-              FACEBOOK
-            </a>
-          </div>
-          <a className="link link-btn copywright">©2025 KRONOS</a>
-        </div>
+        <a>@KRONOS</a>
+        <a
+          className="link-btn"
+          href="https://www.linkedin.com/in/sergio-allushaj-a92b9717a/"
+        >
+          INSTAGRAM
+        </a>
+        <a className="link-btn link" href="https://github.com/kuroiokami89">
+          FACEBOOK
+        </a>
+        {/* <a className="link link-btn copywright">©2025 KRONOS</a> */}
       </footer>
     </>
   );
